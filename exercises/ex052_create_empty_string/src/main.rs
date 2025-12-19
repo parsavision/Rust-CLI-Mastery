9901bd3d63fa2c848fd79a0c2b66368a7c92a028
parsavision <224815858+parsavision@users.noreply.github.com>
2025-12-19 21:52:06 +0330

    add ex 52

---
+++ exercises/ex052_create_empty_string/src/main.rs
@@ -0,0 +1,5 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+}
---
+++ exercises/ex052_create_empty_string/target/debug/deps/ex052_create_empty_string-274ded3a2d2bae53.d
@@ -0,0 +1,5 @@
+/home/parsavisions/Desktop/github/Rust-CLI-Mastery/exercises/ex052_create_empty_string/target/debug/deps/ex052_create_empty_string-274ded3a2d2bae53.d: src/main.rs
+
+/home/parsavisions/Desktop/github/Rust-CLI-Mastery/exercises/ex052_create_empty_string/target/debug/deps/libex052_create_empty_string-274ded3a2d2bae53.rmeta: src/main.rs
+
+src/main.rs:
