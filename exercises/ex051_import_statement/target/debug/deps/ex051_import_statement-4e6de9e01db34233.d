ecb486725e353c9fdaa62057036351904d8effd9
parsavision <224815858+parsavision@users.noreply.github.com>
2025-12-19 20:54:26 +0330

    add ex 51

---
+++ exercises/ex051_import_statement/target/debug/deps/ex051_import_statement-4e6de9e01db34233.d
@@ -0,0 +1,5 @@
+/home/parsavisions/Desktop/github/Rust-CLI-Mastery/exercises/ex051_import_statement/target/debug/deps/ex051_import_statement-4e6de9e01db34233.d: src/main.rs
+
+/home/parsavisions/Desktop/github/Rust-CLI-Mastery/exercises/ex051_import_statement/target/debug/deps/libex051_import_statement-4e6de9e01db34233.rmeta: src/main.rs
+
+src/main.rs:
---
+++ exercises/ex051_import_statement/target/debug/deps/ex051_import_statement-f431345b3c505380.d
@@ -0,0 +1,5 @@
+/home/parsavisions/Desktop/github/Rust-CLI-Mastery/exercises/ex051_import_statement/target/debug/deps/ex051_import_statement-f431345b3c505380.d: src/main.rs
+
+/home/parsavisions/Desktop/github/Rust-CLI-Mastery/exercises/ex051_import_statement/target/debug/deps/libex051_import_statement-f431345b3c505380.rmeta: src/main.rs
+
+src/main.rs:
