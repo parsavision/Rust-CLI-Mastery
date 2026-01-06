c4a69d505e007d8074fdf83e60c24a8bfe3535f5
parsavision <224815858+parsavision@users.noreply.github.com>
2026-01-06 12:33:35 +0330

    add ex 85

--- exercises/ex085_error_recovery/src/main.rs
+++ exercises/ex085_error_recovery/src/main.rs
@@ -3,18 +3,26 @@ use std::io::{self, Write};
 fn main() {
     println!("Exercise 85: Error Recovery");
     println!("Enter a number:");
-    
+
     print!("> ");
     io::stdout().flush().expect("Failed to flush");
-    
+
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
-    
+
     // TODO: Instead of using .expect(), use match to handle parse errors
     // match input.trim().parse::<i32>() {
     //     Ok(num) => println!("You entered: {}", num),
     //     Err(_) => println!("That's not a valid number!"),
     // }
+    match input.trim().parse::<i32>() {
+        Ok(num) => {
+            println!("You entered: {}", num);
+        }
+        Err(_) => {
+            println!("That's not a valid number!");
+        }
+    }
 }
